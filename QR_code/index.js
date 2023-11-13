f36cef4e8d2e3a382be43d4c9065ac873ed7f719
victor simon <121669086+perry-cyber@users.noreply.github.com>
2023-11-13 21:01:42 +0100

    Add files via upload

---
+++ QR_code/index.js
@@ -0,0 +1,22 @@
+// const imageBox = document.getElementById("imgBox");
+// const qrCodeImage = document.getElementById("qrImage");
+// const qrCodeInput = document.getElementById("qrText");
+// const generateQrCode = document.getElementById("generateBtn");
+// const qrContainer = document.querySelector(".container");
+
+
+// const link = "";
+
+// generateQrCode.addEventListener("click", generateQR);
+
+// function generateQR(){
+//     if(qrCodeInput.value.length > 0){
+//     qrCodeImage.setAttribute("src", "link"+ qrCodeInput.value);
+//     imageBox.classList.add("show-img");
+//     }else{
+//         qrContainer.classList.add("error");
+//         setTimeout(() =>{
+//             qrContainer.classList.remove("error")
+//         },2000)
+//     }
+// }
